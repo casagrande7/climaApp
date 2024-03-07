7e68c0340f2bf31e0ab22de9f1b582a0852eb349
casagrande7 <joaofootz10@gmail.com>
2024-03-07 14:02:40 -0300

    Atualizado

--- App.tsx
+++ App.tsx
@@ -3,8 +3,14 @@ import {Image, StyleSheet, Text, View} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
-    <View>
-
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.city}>Presidente Epitácio</Text>
+      </View>
+      <View style={styles.detailsContainer}>
+        <Text style={styles.temperature}>55°C</Text>
+        <Image source={require('./src/assets/imagens/icon3.png')}/>
+      </View>
     </View>
   )
 }
