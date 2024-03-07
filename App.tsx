f1e3c5c6164df702cfcdaa5f8da6848ab3df8de8
casagrande7 <joaofootz10@gmail.com>
2024-03-07 13:52:37 -0300

    At

---
+++ App.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {Image, StyleSheet, Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <View>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  bottomImage: {
+
+  },
+  header: {
+
+  },
+  city: {
+
+  },
+  weatherIcon: {
+
+  },
+  detailsContainer: {
+
+  },
+  weatherCondition: {
+
+  },
+  temperature: {
+
+  },
+  container: {
+
+  },
+  text: {
+
+  }
+  
+});
+
+export default App;
