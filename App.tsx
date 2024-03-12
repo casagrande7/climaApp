e4218af2b8deab2f982eff91dd0c8b5ff5951b1a
casagrande7 <joaofootz10@gmail.com>
2024-03-12 15:40:37 -0300

    Atualizado

--- App.tsx
+++ App.tsx
@@ -1,24 +1,42 @@
-import React, { useState } from "react";
-import {Image, StyleSheet, Text, View} from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 function App(): React.JSX.Element {
 
-const [city, setCity] = useState<string>("São Paulo");
-const [humidity, setHumidity] = useState<string>("15");
-const [condition, setCondition] = useState<string>("Ensolarado");
-const [rainProbability, setRainProbability] = useState<string>("30");
-const [feelsLike, setFeelsLike] = useState<string>("41");
-const [temperature, setTemperature] = useState<string>("37");
-const [night, setNight] = useState<boolean>(false);
+  const [city, setCity] = useState<string>("São Paulo");
+  const [humidity, setHumidity] = useState<string>("15");
+  const [condition, setCondition] = useState<string>("Ensolarado");
+  const [rainProbability, setRainProbability] = useState<string>("30");
+  const [feelsLike, setFeelsLike] = useState<string>("41");
+  const [temperature, setTemperature] = useState<string>("37");
+  const [night, setNight] = useState<boolean>(false);
+
+
+  function isNight() {
+    const hour = new Date().getHours();
+    console.log(hour);
+
+    if (hour >= 18 || hour < 6) {
+      setNight(true);
+    } else {
+      setNight(false);
+    }
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      isNight();
+    }, 6000);
+  })
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, night == false ? styles.containerBgDay : styles.containerBgNight]}>
       <View style={styles.header}>
         <Text style={styles.city}>{city}</Text>
       </View>
       <View style={styles.detailsContainer}>
         <Text style={styles.temperature}>{temperature}°C</Text>
-        <Image source={require('./src/assets/imagens/icon3.png')} style={styles.weatherIcon}/>
+        <Image source={require('./src/assets/imagens/icon3.png')} style={styles.weatherIcon} />
       </View>
       <View>
         <Text style={styles.weatherCondition}>{condition}</Text>
@@ -26,7 +44,7 @@ const [night, setNight] = useState<boolean>(false);
         <Text style={styles.text}>Probabilidade de Chuva: {rainProbability}%</Text>
         <Text style={styles.text}>Umidade: {humidity}%</Text>
       </View>
-      <Image source={require('./src/assets/imagens/cidade.png')} style={styles.bottomImage}/>
+      <Image source={require('./src/assets/imagens/cidade.png')} style={styles.bottomImage} />
     </View>
   )
 }
@@ -41,13 +59,13 @@ const styles = StyleSheet.create({
   },
   header: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     marginBottom: 20
   },
   city: {
     fontWeight: 'bold',
     color: '#fff',
-    fontSize: 25
+    fontSize: 25,
   },
   weatherIcon: {
     width: 100,
@@ -82,8 +100,16 @@ const styles = StyleSheet.create({
     color: '#fff',
     marginBottom: 10,
     fontStyle: 'italic'
+  },
+  containerBgDay: {
+    backgroundColor: '#09d3f3'
+
+  },
+  containerBgNight: {
+    backgroundColor: '#333'
+
   }
-  
+
 });
 
 export default App;
